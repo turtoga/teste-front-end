9efc81e0bef2d352fffda796993caec09850f8d3
turtoga <gabrielalmeida.dev@outlook.com>
2025-03-14 10:36:28 -0300

    add: img e categoria

--- econverse/src/App.tsx
+++ econverse/src/App.tsx
@@ -1,5 +1,7 @@
 import './app.scss'
+import ItemCategoria from './components/Categorias/ItemCategoria'
 import Hero from './components/Hero'
+import img from './assets/img/categorias/supermercados.png'
 
 function App() {
 
@@ -7,6 +9,7 @@ function App() {
     <>
       <main>
         <Hero/>
+        <ItemCategoria img={img} titulo={"Supermercado"}/>
       </main>
     </>
   )
